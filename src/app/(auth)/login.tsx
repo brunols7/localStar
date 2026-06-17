679ac4a1233f21acaf02c2bc04d3126bdc3fd6c2
Bruno Leonardo <bruno.lsilva1508@gmail.com>
2026-06-17 13:33:00 -0300

    feat: login screen

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,17 +1,149 @@
+import { useTheme } from '@/hooks/use-theme'
 import { router } from 'expo-router'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { ChevronLeft, Eye, EyeOff } from 'lucide-react-native'
+import { useState } from 'react'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Login() {
+
+  const handleLogin = () => {
+    console.log('Login with, email:', email, 'password:', password)
+    router.navigate('/')
+  }
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isSecure, setIsSecure] = useState(true)
+
+  const theme = useTheme()
+    
+    const styles = StyleSheet.create({
+      container: {
+          flex: 1,
+          alignItems: 'center',
+          backgroundColor: theme.background,
+          padding: 20
+        },
+        headerContainer: {
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          width: '100%',
+          paddingVertical: 20
+        },
+        titleContainer: {
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+          width: '100%',
+          paddingVertical: 12,
+          gap: 4
+        },
+        title: {
+          fontSize: 20,
+          fontWeight: 'bold',
+          color: theme.text
+        },
+        subtitle: {
+          fontSize: 18,
+          color: theme.textSecondary
+        },
+        formContainer: {
+          marginVertical: 32,
+          flexDirection: 'column',
+          width: '100%',
+          gap: 24
+        },
+        labelContainer: {
+          width: '100%',
+          gap: 4
+        },
+        inputContainer: {
+          flexDirection: 'row',
+          width: '100%',
+          height: 56,
+          backgroundColor: theme.background,
+          borderRadius: 8,
+          borderWidth: 1,
+          borderColor: theme.secondary,
+          alignItems: 'center',
+          paddingHorizontal: 15,
+          gap: 8
+        },
+        loginBtn: {
+          width: '100%',
+          height: 56,
+          backgroundColor: theme.primary,
+          borderRadius: 8,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }
+    })
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View>
-        <Text>Login</Text>
+      <View style={styles.container}>
+
+        <View style={styles.headerContainer}>
+            <TouchableOpacity
+          onPress={() => router.navigate('/')}
+          >
+            <ChevronLeft color={theme.primary} size={32} />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Entrar</Text>
+          <Text style={styles.subtitle}>Acesse sua conta para continuar.</Text>
+        </View>
+
+        <View style={styles.formContainer}>
+
+          <View style={styles.labelContainer}>
+            <Text>E-mail</Text>
+            <TextInput
+              placeholder='Digite seu e-mail'
+              value={email}
+              onChangeText={setEmail}
+              keyboardType='email-address'
+              autoCapitalize='none'
+              style={styles.inputContainer}
+            />
+          </View>
+
+          <View style={styles.labelContainer}>
+            <Text>Senha</Text>
+            
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder='Digite sua senha'
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize='none'
+                secureTextEntry={isSecure}
+                style={{ flex: 1, height: '100%' }}
+              />
+              
+              <TouchableOpacity
+                onPress={() => setIsSecure(!isSecure)}
+              >
+                {isSecure ? <Eye size={22}/> : <EyeOff size={22}/> }
+              </TouchableOpacity>
+
+            </View>
+
+          </View>
+
+        </View>
+
         <TouchableOpacity
-        onPress={() => router.navigate('/')}
+          style={styles.loginBtn}
+          onPress={handleLogin}
         >
-          <Text>Navigate back</Text>
+          <Text style={{ color: 'white', fontWeight: 'semibold', fontSize: 18 }}>Entrar</Text>
         </TouchableOpacity>
+
       </View>
     </SafeAreaView>
   )
--- src/app/(auth)/signup.tsx
+++ src/app/(auth)/signup.tsx
@@ -1,11 +1,25 @@
+import { useTheme } from '@/hooks/use-theme'
 import { router } from 'expo-router'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function SignUp() {
+
+  const theme = useTheme()
+  
+  const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: theme.primary,
+        padding: 20
+      },
+  })
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View>
+      <View style={styles.container}>
         <Text>SignUp</Text>
         <TouchableOpacity
         onPress={() => router.navigate('/')}
@@ -16,5 +30,3 @@ export default function SignUp() {
     </SafeAreaView>
   )
 }
-
-const styles = StyleSheet.create({})
